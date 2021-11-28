5b17fa25235f98956ec9f227b00b4e89647e44a2
Nikita Kryukov <cravtos92@gmail.com>
2021-11-29 01:29:58 +0700

    Add class for creating menus

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import pygame as pg
 import pygame_menu as pgm
@@ -14,6 +15,67 @@ class GameState:
         pass
 
 
+class Menus:
+    """
+    Создает и хранит в себе игровые меню
+    """
+
+    # TODO: Допилить менюшки
+
+    def __init__(self, width: int, height: int):
+        self._height = height
+        self._width = width
+
+        self.stats = self._create_stats()
+        self.settings = self._create_settings()
+        self.main = self._create_main(self.settings, self.stats)
+        self.victory = self._create_victory()
+        self.defeat = self._create_defeat()
+        self.game = self._create_game()
+
+    def resize(self, width: int, height: int):
+        # Подумать: Можно попробовать сократить код, засунув все менюшки в словарь, и обходом по словарю вызывать .resize(). С другой стороны не так уж и много у нас менюшек.
+
+        # Подумать: Возможно неплохо было бы ресайзить только текущее показываемое окно, но тогда нужно будет понять, как узнавать, какое окно сейчас показывается, а так же не забывать ресайзить при переходах между менюшками и их. Звучит довольно запарно. Учитывая, что менюшек у нас не так много, возможно хорошей идеей будет оставить как есть.
+
+        print("resize()")
+        self._width, self._height = width, height
+        self.main.resize(width, height)
+        self.stats.resize(width, height)
+        self.settings.resize(width, height)
+        # self.victory.resize(width, height)
+        # self.defeat.resize(width, height)
+        # self.game.resize(width, height)
+
+    def _create_stats(self):
+        stat = pgm.menu.Menu(title="Hangman", height=self._height, width=self._width)
+        stat.add.button("Назад", pgm.events.BACK)
+        return stat
+
+    def _create_settings(self):
+        settings = pgm.menu.Menu(
+            title="Hangman", height=self._height, width=self._width
+        )
+        settings.add.button("Назад", pgm.events.BACK)
+        return settings
+
+    def _create_main(self, settings, stats):
+        main = pgm.menu.Menu(title="Hangman", height=self._height, width=self._width)
+        main.add.button("Играть", settings)
+        main.add.button("Статистика", stats)
+        main.add.button("Выйти", pgm.events.EXIT)
+        return main
+
+    def _create_game(self):
+        return None
+
+    def _create_victory(self):
+        return None
+
+    def _create_defeat(self):
+        return None
+
+
 class Game:
     """
     Обрабатывает выборы игрока относительно игры
@@ -23,21 +85,9 @@ class Game:
     def __init__(self):
         pg.init()
         pg.display.set_caption("Hangman")
-
         self._size = self._width, self._height = 640, 400
         self._surface = pg.display.set_mode(self._size, pg.RESIZABLE)
-
-        self._stat_menu = pgm.menu.Menu(
-            title="Hangman", height=self._height, width=self._width
-        )
-        self._stat_menu.add.button("Back", pgm.events.BACK)
-
-        self._menu = pgm.menu.Menu(
-            title="Hangman", height=self._height, width=self._width
-        )
-        self._menu.add.button("Play", self._stat_menu)
-        self._menu.add.button("Quit", pgm.events.EXIT)
-
+        self._menus = Menus(self._width, self._height)
         self._running = True
 
     def on_event(self, event):
@@ -47,17 +97,8 @@ class Game:
         if event.type == pg.VIDEORESIZE:
             print("on_event(): pg.VIDEORESIZE")
             self._surface = pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
-            self.on_resize()
-
-    @staticmethod
-    def cleanup():
-        print("cleanup()")
-        pg.quit()
-
-    def on_resize(self) -> None:
-        print("on_resize()")
-        self._size = self._width, self._height = self._surface.get_size()
-        self._menu.resize(self._width, self._height)
+            self._size = self._width, self._height = event.w, event.h
+            self._menus.resize(event.w, event.h)
 
     def run(self):
         while self._running:
@@ -65,12 +106,11 @@ class Game:
             for event in events:
                 self.on_event(event)
 
-            if self._menu.is_enabled():
-                self._menu.update(events)
-                self._menu.draw(self._surface)
+            if self._menus.main.is_enabled():
+                self._menus.main.update(events)
+                self._menus.main.draw(self._surface)
 
             pg.display.update()
-        self.cleanup()
 
 
 def main():
