d659c620b07e2041ebf48e4fb18dfe553a31280f
Nikita Kryukov <cravtos92@gmail.com>
2021-12-28 15:49:29 +0700

    clean up code

--- hangman/conditions.py
+++ hangman/conditions.py
@@ -6,7 +6,6 @@ class Difficulty(Enum):
     Сложность игры
     TODO: Обозначить, какие условия на каких сложностях
     """
-
     EASY = 1
     MEDIUM = 2
     HARD = 3
@@ -16,7 +15,6 @@ class Categories(Enum):
     """
     Категории слов для угадывания
     """
-
     ANIMALS = 1
     BIRDS = 2
     CHEMISTRY = 3
@@ -24,61 +22,69 @@ class Categories(Enum):
     FOOD = 5
     FRUITS = 6
 
-
-ALL_CATEGORIES = {
+CATEGORIES_NAMES = ["ANIMALS", "BIRDS", "CHEMISTRY", "COUNTRIES", "FOOD", "FRUITS"]
+ALL_CATEGORIES = [
     Categories.ANIMALS,
     Categories.BIRDS,
     Categories.CHEMISTRY,
     Categories.COUNTRIES,
     Categories.FOOD,
     Categories.FRUITS,
-}
-
+]
 
 class Conditions:
     def __init__(
         self,
         difficulty: Difficulty,
         categories: set[Categories],
-        cond_timer: bool,
-        cond_hint: bool,
+        has_timer: bool,
+        has_hint: bool,
     ):
         self._difficulty = difficulty
         self._categories = categories
-        self._cond_timer = cond_timer
-        self._cond_hint = cond_hint
+        self._has_timer = has_timer
+        self._has_hint = has_hint
 
-    def set_cond_timer(self, cond_timer):
-        self._cond_timer = cond_timer
+    def add_category(self, category: Categories):
+        print("[dbg] added category ", category)
+        self.categories.add(category)
 
-    def set_cond_hint(self, cond_hint):
-        self._cond_hint = cond_hint
+    def delete_category(self, category: Categories):
+        print("[dbg] removed category ", category)
+        self.categories.discard(category)
 
-    def set_difficulty(self, value: Difficulty):
-        self._difficulty = value
-
-    def add_category(self, value: Categories):
-        self.categories.add(value)
-
-    def delete_category(self, value: Categories):
-        self.categories.discard(value)
-
-    def set_category(self, value: Categories):
+    def set_category(self, category: Categories):
+        print("[dbg] set category ", category)
         self.categories.clear()
-        self.categories.add(value)
-
-    @property
-    def difficulty(self) -> Difficulty:
-        return self._difficulty
+        self.categories.add(category)
 
     @property
     def categories(self) -> set(Categories):
         return self._categories
 
     @property
-    def timer(self) -> bool:
-        return self._cond_timer
+    def difficulty(self) -> Difficulty:
+        return self._difficulty
+    
+    @difficulty.setter
+    def difficulty(self, difficulty: Difficulty):
+        print("[dbg] difficulty set to ", difficulty)
+        self._difficulty = difficulty
 
     @property
-    def hint(self) -> bool:
-        return self._cond_hint
+    def has_timer(self) -> bool:
+        return self._has_timer
+
+    @has_timer.setter
+    def has_timer(self, has_timer: bool):
+        print("[dbg] has_timer set to ", has_timer)
+        self._has_timer = has_timer
+
+    @property
+    def has_hint(self) -> bool:
+        return self._has_hint
+
+    @has_hint.setter
+    def has_hint(self, has_hint: bool):
+        print("[dbg] has_hint set to ", has_hint)
+        self._has_hint = has_hint
